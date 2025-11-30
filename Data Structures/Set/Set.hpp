3f2919a1cc8d7f4b441f3f9c291fd77202ce6e71
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2025-11-30 14:50:09 +0200

    Refactor Set class with proper member functions

--- Data Structures/Set/Set.hpp
+++ Data Structures/Set/Set.hpp
@@ -2,85 +2,10 @@
 
 class Set {
 public:
-	~Set() {
-		free(root);
-		root = nullptr;
-	}
-
-	bool contains(int toFind) const {
-		Node* curr = root;
-
-		while (curr) {
-			if (curr->data == toFind) {
-				return true;
-			}
-			else if (toFind < curr->data) {
-				curr = curr->left;
-			}
-			else {
-				curr = curr->right;
-			}
-		}
-
-		return false;
-	}
-	bool insert(int toAdd) {
-		Node** curr = &root;
-
-		while (*curr) {
-			if ((*curr)->data == toAdd) {
-				return false;
-			}
-			else if (toAdd < (*curr)->data) {
-				curr = &(*curr)->left;
-			}
-			else {
-				curr = &(*curr)->right;
-			}
-		}
-
-		*curr = new Node(toAdd);
-		return true;
-	}
-	bool remove(int toDelete) {
-		Node** curr = &root;
-
-		while (*curr)
-		{
-			if ((*curr)->data == toDelete) {
-				//delete the element
-
-				if (!(*curr)->left && !(*curr)->right) //if its a leaf!
-				{
-					delete *curr;
-					*curr = nullptr;
-				}
-				else if (!(*curr)->right) //only left
-				{
-					Node* toDeleteNode = *curr;
-					*curr = (*curr)->left;
-					delete toDeleteNode;
-				}
-				else if (!(*curr)->left) //only right
-				{
-					Node* toDeleteNode = *curr;
-					*curr = (*curr)->right;
-					delete toDeleteNode;
-				}
-				else //if we have two children 
-				{
-
-				}
-			}
-			else if (toDelete < (*curr)->data) {
-				curr = &(*curr)->left;
-			}
-			else {
-				curr = &(*curr)->right;
-			}
-		}
-		return false; //nothing to delete
-	}
+	~Set();
+	bool contains(int toFind) const;
+	bool insert(int toAdd);
+	bool remove(int toDelete);
 
 private:
 	struct Node {
@@ -88,30 +13,123 @@ private:
 		Node* right;
 		int data;
 
-		Node(int data, Node* left = nullptr, Node* right = nullptr):
-			data(data), left(left), right(right) { }
+		Node(int data, Node* left = nullptr, Node* right = nullptr);
 	};
 
 	Node* root = nullptr;
 
-	void free(Node* root) {
-		if (!root) {
-			return;
-		}
-		
-		free(root->left);
-		free(root->right);
-		delete root;
-	}
-	Node* copyFrom(const Node* root) {
-		if (!root) {
-			return nullptr;
-		}
-
-		Node* result = new Node(root->data);
-		result->left = copyFrom(root->left);
-		result->right = copyFrom(root->right);
-		
-		return result;
-	}
+	void free(Node* root);
+	Node* copyFrom(const Node* root);
 };
+
+// ---------------- DEFINITIONS ----------------
+
+Set::~Set() {
+	free(root);
+	root = nullptr;
+}
+
+bool Set::contains(int toFind) const {
+	Node* curr = root;
+
+	while (curr) {
+		if (curr->data == toFind) {
+			return true;
+		}
+		else if (toFind < curr->data) {
+			curr = curr->left;
+		}
+		else {
+			curr = curr->right;
+		}
+	}
+
+	return false;
+}
+
+bool Set::insert(int toAdd) {
+	Node** curr = &root;
+
+	while (*curr) {
+		if ((*curr)->data == toAdd) {
+			return false;
+		}
+		else if (toAdd < (*curr)->data) {
+			curr = &(*curr)->left;
+		}
+		else {
+			curr = &(*curr)->right;
+		}
+	}
+
+	*curr = new Node(toAdd);
+	return true;
+}
+
+bool Set::remove(int toDelete) {
+	Node** curr = &root;
+
+	while (*curr)
+	{
+		if ((*curr)->data == toDelete) {
+			//delete the element
+
+			if (!(*curr)->left && !(*curr)->right) //if its a leaf!
+			{
+				delete *curr;
+				*curr = nullptr;
+			}
+			else if (!(*curr)->right) //only left
+			{
+				Node* toDeleteNode = *curr;
+				*curr = (*curr)->left;
+				delete toDeleteNode;
+			}
+			else if (!(*curr)->left) //only right
+			{
+				Node* toDeleteNode = *curr;
+				*curr = (*curr)->right;
+				delete toDeleteNode;
+			}
+			else //if we have two children 
+			{
+
+			}
+		}
+		else if (toDelete < (*curr)->data) {
+			curr = &(*curr)->left;
+		}
+		else {
+			curr = &(*curr)->right;
+		}
+	}
+	return false; //nothing to delete
+}
+
+// Node constructor
+Set::Node::Node(int data, Node* left, Node* right) :
+	data(data), left(left), right(right) { }
+
+// Free helper
+void Set::free(Node* root) {
+	if (!root) {
+		return;
+	}
+
+	free(root->left);
+	free(root->right);
+	delete root;
+}
+
+// Copy helper
+Set::Node* Set::copyFrom(const Node* root) {
+	if (!root) {
+		return nullptr;
+	}
+
+	Node* result = new Node(root->data);
+	result->left = copyFrom(root->left);
+	result->right = copyFrom(root->right);
+
+	return result;
+}

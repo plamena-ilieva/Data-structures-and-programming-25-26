09fafe5138232441feeab8aeecf4b053d12c1f33
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2025-12-12 16:36:17 +0200

    Update Set.hpp

--- Data Structures/Set/Set.hpp
+++ Data Structures/Set/Set.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <iostream>
-
+#include <stack>
 
 class Set {
 public:
@@ -17,8 +17,43 @@ private:
 		Node* right;
 		int data;
 
+		size_t subTreeSize = 1; //
+
+		static size_t getTreeSize(const Node* tree)
+		{
+			return tree ? tree->subTreeSize : 0;
+		}
+
 		Node(int data, Node* left = nullptr, Node* right = nullptr);
 	};
+
+	const Node* getNodeByIndex(const Node* root, size_t index) const
+	{
+		size_t leftSize = Node::getTreeSize(root->left);
+		if (index == leftSize)
+			return root;
+		if (index < leftSize)
+			return getNodeByIndex(root->left, index);
+		else
+			return getNodeByIndex(root->right, index - leftSize - 1);
+
+	}
+
+	long long getRank(const Node* root, int toRank) const {
+		if (!root) {
+			throw std::invalid_argument("invalid element");
+		}
+		if (root->data == toRank) {
+			return Node::getTreeSize(root->left);
+		}
+		if (toRank < root->data) {
+			return getRank(root->left, toRank);
+		}
+		long long leftSize = Node::getTreeSize(root->left);
+		long long rightIndex = getRank(root->right, toRank);
+		return leftSize + rightIndex + 1;
+	}
+
 	Node** getMin(Node** curr)
 	{
 		while ((*curr)->left)
@@ -31,9 +66,72 @@ private:
 
 	void free(Node* root);
 	Node* copyFrom(const Node* root);
+public:
+
+	class ConstForwardIterator {
+
+		void pushLeft(const Set::Node* curr)
+		{
+			while (curr)
+			{
+				st.push(curr);
+				curr = curr->left;
+			}
+		}
+	public:
+		ConstForwardIterator(const Set::Node* root)
+		{
+			pushLeft(root);
+		}
+		ConstForwardIterator& operator++() //go to next el
+		{
+			const Node* currEl = st.top();
+			st.pop();
+			if (currEl->right)
+				pushLeft(currEl->right);
+			return *this;
+		}
+
+		bool operator==(const ConstForwardIterator& other)
+		{
+			return st == other.st;
+		}
+
+		bool operator!=(const ConstForwardIterator& other)
+		{
+			return st != other.st;
+		}
+
+		int operator*() const //give element that we point to
+		{
+			return st.top()->data;
+		}
+	private:
+		std::stack<const Node*> st; //top of the stack will be my curr el
+	};
+
+	ConstForwardIterator begin() const
+	{
+		return ConstForwardIterator(root);
+	}
+	ConstForwardIterator end() const
+	{
+		return ConstForwardIterator(nullptr);
+	}
+
+
+
+	int operator[](size_t index) const
+	{
+		return getNodeByIndex(root, index)->data;
+	}
+
+	long long getRank(int el) const {
+		return getRank(root, el);
+	}
 };
 
-Set::Set(const Set& other){
+Set::Set(const Set& other) {
 	root = copyFrom(other.root);
 }
 
@@ -64,6 +162,8 @@ bool Set::insert(int toAdd) {
 	Node** curr = &root;
 
 	while (*curr) {
+		(*curr)->subTreeSize++; //assume that insert is succesfull 
+
 		if ((*curr)->data == toAdd) {
 			return false;
 		}
@@ -153,4 +253,24 @@ Set::Node* Set::copyFrom(const Node* root) {
 	result->right = copyFrom(root->right);
 
 	return result;
+}
+
+int main()
+{
+	Set s;
+
+	int arr[] = { 5,1,3,2,6,8,7,9,4,0 };
+	for (int i = 0; i < 10; i++)
+		s.insert(arr[i]);
+
+	for (int i = 0; i < 10; i++)
+		std::cout << s[i] << std::endl;
+
+	for (int i = 0; i < 10; i++)
+		std::cout << s.getRank(i) << std::endl;
+
+	for (auto it = s.begin(); it != s.end(); ++it)
+	{
+		std::cout << (*it);
+	}
 }
